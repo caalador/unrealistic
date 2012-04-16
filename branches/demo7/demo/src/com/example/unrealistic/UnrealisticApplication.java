290ad56db759b8b5a4f2fb61ba7de67a640ee732
mikael.grankvist@gmail.com <mikael.grankvist@gmail.com@56464933-2dbd-baad-843b-f9d5b06723a1>
2012-04-16 18:40:19 +0000

    Vaadin 7 alpha2 version of the demo application

---
+++ branches/demo7/demo/src/com/example/unrealistic/UnrealisticApplication.java
@@ -0,0 +1,116 @@
+package com.example.unrealistic;
+
+import org.percepta.mgrankvi.unrealistic.ConductorCell;
+import org.percepta.mgrankvi.unrealistic.UnrealisticComponent;
+import org.percepta.mgrankvi.unrealistic.client.ui.data.LimitChange;
+
+import com.example.unrealistic.demos.CFDemo;
+import com.example.unrealistic.demos.TimedCircleAndFlame;
+import com.vaadin.Application;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.NativeSelect;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+public class UnrealisticApplication extends Application {
+
+	private static final long serialVersionUID = 3694512868981467643L;
+
+	private Window mainWindow;
+	private final HorizontalLayout contentLayout = new HorizontalLayout();
+
+	@Override
+	public void init() {
+		mainWindow = new Window("Unrealistic Application");
+		((VerticalLayout) mainWindow.getContent()).setSpacing(true);
+		setMainWindow(mainWindow);
+
+		selectLayout();
+
+		mainWindow.addComponent(contentLayout);
+	}
+
+	private void selectLayout() {
+		final NativeSelect select = new NativeSelect();
+		select.addItem("Blue");
+		select.addItem("Flame");
+		select.addItem("Long Flame");
+		select.addItem("Long Flame reverse");
+		select.addItem("Long Flame loop position set");
+		select.addItem("Circle");
+		select.addItem("Circle with fade");
+		select.addItem("Circle flame timing");
+		select.addItem("Demo...");
+		select.setImmediate(true);
+		select.addListener(new ValueChangeListener() {
+			private static final long serialVersionUID = -838069154357449340L;
+
+			public void valueChange(final ValueChangeEvent event) {
+				contentLayout.removeAllComponents();
+				final String selection = (String) event.getProperty().getValue();
+				if ("Blue".equals(selection)) {
+					final UnrealisticComponent component = createAndAddComponent(150, 150);
+					component.addImage("demo/images/TestFlame.png", 50, 50, 10, 1);
+					component.setFps(10);
+				} else if ("Flame".equals(selection)) {
+					final ConductorCell cell = new ConductorCell("demo/images/FlameSprites.png", 50, 50, 14, 1);
+					final UnrealisticComponent component = createAndAddComponent(150, 150);
+					component.setFps(10);
+					component.addImage(cell);
+				} else if ("Long Flame".equals(selection)) {
+					final ConductorCell cell = new ConductorCell("demo/images/LongerFlame.png", 50, 50, 14, 2);
+					final UnrealisticComponent component = createAndAddComponent(150, 150);
+					component.setFps(10);
+					component.addImage(cell);
+				} else if ("Long Flame reverse".equals(selection)) {
+					final ConductorCell cell = new ConductorCell("demo/images/LongerFlame.png", 50, 50, 14, 2);
+					cell.setReverse(true);
+					final UnrealisticComponent component = createAndAddComponent(150, 150);
+					component.setFps(10);
+					component.addImage(cell);
+				} else if ("Long Flame loop position set".equals(selection)) {
+					final ConductorCell cell = new ConductorCell("demo/images/LongerFlame.png", 50, 50, 14, 2);
+					cell.setLoopStartColumn(7);
+					final UnrealisticComponent component = createAndAddComponent(150, 150);
+					component.setFps(10);
+					component.addImage(cell);
+				} else if ("Circle".equals(selection)) {
+					final ConductorCell cell = new ConductorCell("demo/images/CircleSprites.png", 90, 90, 29, 5);
+					cell.setLoop(false);
+					cell.setOffsetX(23);
+					cell.setOffsetY(30);
+					final UnrealisticComponent component = createAndAddComponent(150, 150);
+					component.addImage(cell);
+				} else if ("Circle with fade".equals(selection)) {
+					final ConductorCell cell = new ConductorCell("demo/images/CircleSprites.png", 90, 90, 29, 5);
+					cell.setLoop(false);
+					cell.setOffsetX(23);
+					cell.setOffsetY(30);
+					final LimitChange limit = new LimitChange(145);
+					limit.setFadeOut(5000);
+					cell.addLimit(limit);
+					final UnrealisticComponent component = createAndAddComponent(150, 150);
+					component.addImage(cell);
+				} else if ("Circle flame timing".equals(selection)) {
+					contentLayout.addComponent(new TimedCircleAndFlame());
+				} else if ("Demo...".equals(selection)) {
+					contentLayout.addComponent(new CFDemo());
+				}
+			}
+		});
+		mainWindow.addComponent(select);
+	}
+
+	private UnrealisticComponent createAndAddComponent(final int width, final int height) {
+		final UnrealisticComponent component = new UnrealisticComponent();
+		component.setWidth(width + "px");
+		component.setHeight(height + "px");
+		component.setFps(15);
+
+		contentLayout.addComponent(component);
+		return component;
+	}
+
+}
